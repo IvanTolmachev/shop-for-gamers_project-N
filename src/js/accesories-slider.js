6a25c2a1bce07156f2cfec17d6fe2232890d01f4
Vyalov Vadim <vyalov.vadim@gmail.com>
2022-11-23 10:51:37 +0200

    slider

---
+++ src/js/accesories-slider.js
@@ -0,0 +1,36 @@
+var a_swiper = new Swiper('.accesories-swiper', {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        grid: {
+          rows: 6,
+        },
+        breakpoints: {
+          768: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            grid: {
+              rows: 6,
+            },
+            spaceBetween: 38,
+          },
+
+          1280: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+            grid: {
+              rows: 4,
+            },
+            spaceBetween: 20,
+          },
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+  },
+  
+});
+
+a_swiper.on('resize', function () {
+  // console.log('slide changed');
+  a_swiper.update()
+});
