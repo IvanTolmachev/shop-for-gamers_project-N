a031143beb6a41174efe190672f197b3f10987ec
IvanTolmachev <39563900+IvanTolmachev@users.noreply.github.com>
2022-11-26 11:54:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import './js/modal';
-import './js/menu';
+// import './js/modal';
+// import './js/menu';
 // import './js/dropdown';
 // import './js/swiper-slider';
 // import './js/accesories-slider';
